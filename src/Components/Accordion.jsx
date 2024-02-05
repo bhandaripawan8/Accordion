3c188410ff24c7d54436fb1f5c3e4566a0ca3e8f
Pawan <pawanb78@gmail.com>
2024-02-05 22:24:58 +0000

    single selection expanding answer working

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,23 +1,30 @@
 import Data from './Data';
 import './Accordion.css'
+import { useState } from 'react';
 
 export default function Accordion(){
-
+    const [selectedid, setselectedid] = useState(null);
+    function expandHeader(getselectedId){
+        console.log(getselectedId);
+        setselectedid(getselectedId === selectedid ? null: getselectedId);
+    }
  return(
     <>
+    { Data.length > 0 ? 
     <div className="Accordion-container">
         <button>Select multiple</button>
             {Data.map((item) =>(
                 <div className='contents' key={item.id}>
-                    <div className="question">
+                    <div className="question" onClick={()=>expandHeader(item.id)}>
                         <h3>{item.question}</h3>
+                        <span>+</span>
                     </div>
                     <div className="answer">
-                        {item.answer}
+                       {selectedid === item.id ? <div>{item.answer}</div>: null}
                     </div>
                 </div>
             ))}
-    </div>
+    </div> : <div><h2>No Data found...</h2></div>}
     </>
  )
 }
